f56f66ab9ee165ce899002db72e4a848fc977422
sulaim <sulaimsaed@gmail.com>
2023-11-13 18:13:05 +0530

    Implemented a function for generating virtual bisuness card

---
+++ vcard.py
@@ -0,0 +1,33 @@
+import csv
+import os
+import sys
+
+
+def get_data(filename):
+    with open(filename, "r") as data:
+        detail = csv.reader(data)
+        os.mkdir("vcards")
+        for i in detail:
+            lname, fname, designation, email, phone = i
+            print(i)
+            with open(f"vcards/{lname}.txt", "w") as d:
+                d.write(
+                    f"""
+BEGIN:VCARD
+VERSION:2.1
+N:{lname};{fname}
+FN:{fname} {lname}
+ORG:Authors, Inc.
+TITLE:{designation}
+TEL;WORK;VOICE:{phone}
+ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+EMAIL;PREF;INTERNET:{email}
+REV:20150922T195243Z
+END:VCARD
+"""
+                )
+
+
+if __name__ == "__main__":
+    f = sys.argv[1]
+    get_data(f)
