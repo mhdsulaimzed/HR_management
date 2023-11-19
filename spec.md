36a561f5aa7b2874159c275b58bd5918703be638
sulaim <sulaimsaed@gmail.com>
2023-11-20 01:25:33 +0530

    implemented fuctons for creating database , fetch from database

--- spec.md
+++ spec.md
@@ -3,28 +3,60 @@
  
  
  
-# Purpose:
+Certainly! Below is an improved version of your `spec.md` with some formatting enhancements:
 
-The actuall purpose of this code is to read informations of employees from csv file and make a virtual visiting card files for each person,Parse command-line arguments for the vcard.py program.
+```markdown
+# Objective
+
+Create a Python script that converts data from a CSV file into vCard Virtual Visiting Card files.
+
+## Purpose
+
+The purpose of this code is to:
+
+1. Read employee information from a CSV file.
+2. Create a database in PostgreSQL.
+3. Load the employee details into the database.
+4. Generate virtual visiting cards and QR code image files for each person.
+
+## Input
+
+The script is invoked using the following command-line format:
+
+```bash
+python3 vcard.py <action> [options]
+```
+
+Available actions:
+
+- `createdb`: Create a PostgreSQL database.
+- `load`: Load CSV file data into the database.
+- `vcard`: Generate virtual visiting cards.
+- `qrcode`: Generate QR codes.
+
+Options:
+
+- `-b, --db`: Specify the database name.
+- `-l, --load`: Specify the CSV file (only with the `load` action).
+- `-t, --table`: Specify the table name (only with `load`, `vcard`, and `qrcode` actions).
+- `-v, --verbose` (optional): Print verbose logs.
+- `-s, --size` (optional): Size of the QR code to generate (default: 300, only with the `qrcode` action).
+- `-d, --address` (optional): Add custom address for the vCard; default value can be set (only with the `load` action).
+
+## Output
+
+### `createdb` and `load` Actions
+
+Create a PostgreSQL database and load the CSV file into the database with a user-input table name.
+
+### `vcard` and `qrcode` Actions
+
+Create directories named `vcards` and `qrcode` and save the generated vCards and QR codes in these directories. Files will be named with the employee's last name in VCF and PNG formats.
+
+## Usage
+
+Run the script through the command line by choosing one of the actions: `createdb`, `load`, `vcard`, or `qrcode`. Perform tasks such as creating a database, loading data from the employee's CSV file into the database, and generating vCards and QR codes.
 
 
-# Input :
-
-inputs a csv file containing information of employees through commandline
-`python3 vcard.py <filename>`
-filename (required): The name of the CSV file to process.
--`-v, --verbose` (optional): Whether to print verbose logs.
--`-a, --all` (optional): Whether to generate both QR and vCard files.
--`-s, --size` (optional): The size of the QR code to generate (default: 300).
--`-ad, --address` (optional): For adding custom address  for the vcard and default value can be set as given.
-
-# Output : 
-
-This will create directories named `vcards` and `qrcode` and save the generated vCards and QR codes in these directories
-and the files will be having lastname of the employee in vcf and png format
-
-# Usage :
-
-This script should be executed through command line by passing csv-filename as an argument and returns files with 
 
 
--- vcard.py
+++ vcard.py
@@ -1,3 +1,5 @@
+import psycopg2
+
 import argparse
 import csv
 import logging
@@ -8,23 +10,16 @@ import requests
 logger = None
 
 
-def setup_logging(log_level):
+def configure_logger(log_level):
     global logger
-    logger = logging.getLogger("SheetGen")
+    formatter = "[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"
+    logger = logging.getLogger(__name__)
     handler = logging.StreamHandler()
     fhandler = logging.FileHandler("run.log")
-    fhandler.setLevel(logging.DEBUG)
     handler.setLevel(log_level)
-    handler.setFormatter(
-        logging.Formatter(
-            "[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"
-        )
-    )
-    fhandler.setFormatter(
-        logging.Formatter(
-            "[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"
-        )
-    )
+    fhandler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter(formatter))
+    fhandler.setFormatter(logging.Formatter(formatter))
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
     logger.addHandler(fhandler)
@@ -33,31 +28,45 @@ def setup_logging(log_level):
 def parse_args():
     parser = argparse.ArgumentParser(
         prog="vcard.py",
-        description="Generates Vcards and Qr codes for employees in csv files as vcf and png format respectively",
+        description="""
+        ---------------------------------------------------------------------------------------------------
+        This Program is designed to do HR management operations like 
+        -initialise a database in postgres SQL
+        -load data from csv file  
+        -generate vcard 
+        -generate qrcode 
+
+        ---------------------------------------------------------------------------------------------------
+        """,
         epilog="use these options to do specific fuctions as  written above",
     )
-    parser.add_argument("filename", help="name of the csv file as input, this is *required")
+    parser.add_argument(
+        "action",
+        help="Operations to be performed Choose from above ",
+        choices=[ "createdb","loadcsv", "vcard", "qrcode"],
+    )
+    parser.add_argument("-b", "--db", help="Specify database name / give a name for a new database", type=str)
+    parser.add_argument(
+        "-l", "--load", help="Specifies the file name to be loaded (for the 'loadcsv' action)", type=str
+    )
+    parser.add_argument(
+        "-t", "--table", help=" Specifies the table name (default is 'employees')", type=str, default="employees"
+    )
     parser.add_argument(
         "-v", "--verbose", help="print out detailed logs", action="store_true"
     )
+   
+    
     parser.add_argument(
-        "-a",
-        "--all",
-        help="Print both qr and vcard file",
-        action="store_true",
-        default=False,
+        "-s", "--size", help= "Specifies a custom size between 100 and 500 (for the 'qrcode' action)", default=300
     )
     parser.add_argument(
-        "-s", "--size", help="Add custom size between 100 and 500", default=300
-    )
-    parser.add_argument(
-        "-ad",
+        "-d",
         "--address",
         help="Add new address",
         type=str,
         default="100 Flat Grape Dr.;Fresno;CA;95555;United States of America",
     )
-    parser.add_argument()
 
     args = parser.parse_args()
     return args
@@ -71,6 +80,27 @@ def parse_data(filename):
             datalist.append(i)
 
     return datalist
+
+
+def fetch_from_db(dbname, tname ,user):
+    print(dbname, tname)
+    connection = psycopg2.connect(f"dbname={dbname} user={user}")
+    curs = connection.cursor()
+    curs.execute(
+        f"""SELECT first_name,last_name,designation,email,phone,company_address 
+                     FROM
+                     {tname} """
+    )
+    tup_datas = curs.fetchall()
+    data = []
+    for i in tup_datas:
+        data.append(list(i))
+
+    connection.commit()
+    curs.close()
+    connection.close()
+    logger.info(f"Fetched all datas from {tname} table in {dbname} database")
+    return data
 
 
 def generate_vcard_content(lname, fname, designation, email, phone, address):
@@ -88,52 +118,134 @@ END:VCARD
 """
 
 
-def generate__vcards(data, address):
+def generate_qrcode(dimension, i):
+    qr_code = requests.get(
+        f"https://chart.googleapis.com/chart?cht=qr&chs={dimension}x{dimension}&chl={i}"
+    )
+    return qr_code
+
+
+def create__vcards(data):
     os.mkdir("vcards")
     for i in data:
-        lname, fname, designation, email, phone = i
+        lname, fname, designation, email, phone, address = i
+        vcard = generate_vcard_content(lname, fname, designation, email, phone, address)
         with open(f"vcards/{lname}.vcf", "w") as d:
-            d.write(
-                generate_vcard_content(lname, fname, designation, email, phone, address)
-            )
+            d.write(vcard)
         logger.debug("Created vcard for %s ", lname)
     logger.info("Created all vcards")
 
 
-def generate_qr_code(data, dimension):
+def create_qrcode_images(data, dimension):
     os.mkdir("qrcode")
     print(type(dimension))
 
     for i in data:
-        qr_code = requests.get(
-            f"https://chart.googleapis.com/chart?cht=qr&chs={dimension}x{dimension}&chl={i}"
-        )
-
         with open(f"qrcode/{i[0]}.qr.png", "wb") as Q:
+            qr_code = generate_qrcode(dimension, i)
             Q.write(qr_code.content)
         logger.debug("Created QRcode for %s", i[0])
     logger.info("Created QRs for all")
 
 
+def create_database(dbname,user):
+    connection = psycopg2.connect(database="postgres", user=user)
+    curs = connection.cursor()
+    curs.execute("commit")
+    curs.execute(f"create database {dbname}")
+    curs.close()
+    connection.close()
+    logger.info("Database created")
+
+
+def create_table(dbname, tname,user):
+    print(dbname, tname)
+    connnection = psycopg2.connect(f"dbname={dbname} user={user}")
+    curs = connnection.cursor()
+    curs.execute(
+        f"""CREATE TABLE {tname}(
+                       s_no BIGSERIAL PRIMARY KEY,
+                       first_name VARCHAR(50),
+                       last_name VARCHAR(50) ,
+                       designation VARCHAR(50), 
+                       email VARCHAR(50),
+                        phone VARCHAR(50), 
+                       company_address VARCHAR(100) );"""
+    )
+
+    connnection.commit()
+    curs.close()
+    connnection.close()
+    logger.info(f"Table {tname} created in {dbname} database")
+
+
+def load_csv_into_db(dbname, tname, data, address,user):
+    connection = psycopg2.connect(f"dbname={dbname} user={user}")
+    curs = connection.cursor()
+    fname, lname, designation, email, phone = data
+    curs.execute(
+        f"""INSERT
+                     INTO 
+                     {tname}(first_name, last_name, designation, email, phone, company_address)
+                      VALUES(%s,%s,%s,%s,%s,%s)""",
+        (fname, lname, designation, email, phone, address),
+    )
+
+    connection.commit()
+    curs.close()
+    connection.close()
+    logger.debug(f" Inserted datas of {fname} into table{tname} ")
+
+
 def main():
     args = parse_args()
-    dimension = args.size
-    address = args.address
+    user = os.getenv("USERNAME")
 
     if args.verbose:
-        setup_logging(logging.DEBUG)
+        configure_logger(logging.DEBUG)
     else:
-        setup_logging(logging.INFO)
+        configure_logger(logging.INFO)
+    if args.action == "createdb":
+        dbname = args.db
+        print(args.action)
+        create_database(dbname,user)
 
-    file = args.filename
-    data = parse_data(file)
+    if args.action == "loadcsv":
+        if args.load:
+            address = args.address
+            dbname = args.db
+            file = args.load
+            tname = args.table
+            datas = parse_data(file)
+            create_table(dbname, tname, user)
+            print(len(datas))
 
-    generate__vcards(data, address)
-    if args.all:
-        if not dimension.isnumeric() or int(dimension) > 500:
+            for i in range(len(datas)):
+                data = datas[i]
+                load_csv_into_db(dbname, tname, data, address ,user)
+            logger.info(f"Loaded all datas into database")
+
+    if args.action == "vcard":
+        print(user)
+        dbname = args.db
+        tname = args.table
+        data = fetch_from_db(dbname, tname ,user)
+        create__vcards(data)
+
+    if args.action == "qrcode":
+        dbname = args.db
+        tname = args.table
+        dimension = args.size
+
+        if type(dimension) != int or int(dimension) > 500:
             logger.error("size must be an integer between 100 - 500")
         else:
-            generate_qr_code(data, dimension)
+            data = fetch_from_db(dbname,tname,user)
+            create_qrcode_images(data, dimension)
+   
+        
+    
+        
 
 
 if __name__ == "__main__":
