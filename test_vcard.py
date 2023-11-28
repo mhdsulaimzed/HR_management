0de4b229ecee4037761cbdd8440db516084a83da
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-11-28 11:54:16 +0530

    Uses mock to adjust call signatures

--- test_vcard.py
+++ test_vcard.py
@@ -1,8 +1,11 @@
+import mock
+
 from hr import parse_data,generate_vcard_content
 
 def test_parsed_data():
-    file = "test_files/test_sample.csv"
-    assert parse_data(file) == [['Campbell', 'Pamela', 'Hydrologist', 'pamel.campb@wilson.com', '001-371-295-6798x4774'],
+    args = mock.Mock()
+    args.load = "test_files/test_sample.csv"
+    assert parse_data(args) == [['Campbell', 'Pamela', 'Hydrologist', 'pamel.campb@wilson.com', '001-371-295-6798x4774'],
                                ['Ayers', 'Adam', 'Clinical cytogeneticist', 'adam.ayers@lopez-wu.biz', '+1-407-869-4881']]
                                 
 
