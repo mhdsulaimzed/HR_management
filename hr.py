af29ac17f5729a10a2e71d096cc6af5cdfd69f33
sulaim <sulaimsaed@gmail.com>
2023-11-29 17:03:29 +0530

    changes as per feedback

--- hr.py
+++ hr.py
@@ -9,6 +9,7 @@ import logging
 import os
 import requests
 import sys
+import shutil
 
 
 logger = None
@@ -39,8 +40,6 @@ def set_db_config(dbname):
     with open('init.ini',"w") as configfile:
         config.write(configfile)
 
-
-
 def parse_args():
     config = ConfigParser()
     config.read("init.ini")
@@ -54,7 +53,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        "-b", "--db", help="Database name", action="store_true" , default=config["Database"]["dbname"]
+        "-b", "--db", help="Database name (Default : %(default)s)", action="store_true" , default=config["Database"]["dbname"]
     )
     parser.add_argument(
         "-v", "--verbose", help="print out detailed logs", action="store_true"
@@ -64,19 +63,13 @@ def parse_args():
 
     # database command
     parser_createdb = subparser.add_parser("createdb", help="create a database")
-    parser_createdb.add_argument("-b", "--db", help="Specify database name ", type=str)
-
+    parser_createdb.add_argument("-b", "--db", help="database name", type=str)
 
     # loading csv into database
     parser_load = subparser.add_parser(
         "loadcsv", help="insert data from file into employee table in database ",description="Imports list of employees into the database from specified file '-l'"
     )
-    parser_load.add_argument(
-        "-l",
-        "--load",
-        help="Specifies the file name to be loaded ",
-        type=str,
-    )
+    parser_load.add_argument("file",help="File name ",type=str,)
    
     parser_load.add_argument(
         "-d",
@@ -85,33 +78,22 @@ def parse_args():
         type=str,
         default="100 Flat Grape Dr.;Fresno;CA;95555;United States of America",
     )
+
     # commad for creating vcards
     parser_vcard = subparser.add_parser("vcard", help="generate vcards")
-    parser_vcard.add_argument("-b", "--db", help="Specify database name ", type=str)
+    
 
     parser_qrcode = subparser.add_parser("qrcode", help="generate qrcode")
     parser_qrcode.add_argument("-s","--size",
-        help="Specifies a custom size between 100 and 500 (for the 'qrcode' command)",
+        help="Qr dimension ",
         default=300,)
 
-    parser_leave_emp = subparser.add_parser("leavemp", help="add leaves for employees",description="Adds leaves taken by the employee")
+    parser_leave_emp = subparser.add_parser("leave-emp", help="add leaves for employees",description="Adds leaves taken by the employee")
 
-    parser_leave_emp.add_argument(
-        "-e", "--empid", help="Specify employee id ", type=str
-    )
-    parser_leave_emp.add_argument(
-        "-d", "--date", help="Specify date ", type=str, default=todays_date
-    )
-    parser_leave_emp.add_argument(
-        "-r",
-        "--reason",
-        help="Specify reason of leave ",
-        type=str,
-        default="Not mentioned",
-    )
-    parser_export = subparser.add_parser(
-        "export", help="Get the employee detail as csv file",description="Get a detailed summary of all employees in a csv file"
-    )
+    parser_leave_emp.add_argument("empid", help="Employee id ", type=str)
+    parser_leave_emp.add_argument("-d", "--date", help="Date 'YYYY-MM-DD' (Default : %(default)s) ", type=str, default=todays_date)
+    parser_leave_emp.add_argument( "-r", "--reason",help=" Reason for leave ",type=str,default="Not mentioned")
+    parser_export = subparser.add_parser("export", help="Get the employee detail as csv file",description="Get a detailed summary of all employees in a csv file")
     
 
     args = parser.parse_args()
@@ -119,7 +101,7 @@ def parse_args():
 
 
 def parse_data(args):
-    filename = args.load
+    filename = args.file
     datalist = []
     try:
         with open(filename, "r") as data:
@@ -176,6 +158,8 @@ def generate_qrcode(dimension, i):
 
 
 def create_vcards(data):
+    if os.path.exists("vcard"):
+        shutil.rmtree("vcard")
     os.mkdir("vcards")
     for i in data:
         lname, fname, designation, email, phone, address = i
@@ -189,15 +173,17 @@ def create_vcards(data):
 def create_qrcode_images(data, args):
     if type(args.size) != int or int(args.size) > 500:
         logger.error("size must be an integer between 100 - 500")
+    
     else:
-        if not os.path.exists("qrcode"):
-            os.mkdir("qrcode")
-            for i in data:
-                with open(f"qrcode/{i[0]}.qr.png", "wb") as Q:
-                    qr_code = generate_qrcode(args.size, i)
-                    Q.write(qr_code.content)
-                logger.debug("Created QRcode for %s", i[0])
-            logger.info("Created QRs for all")
+        if os.path.exists("qrcode"):
+            shutil.rmtree("vcard")
+        os.mkdir("qrcode")
+        for i in data:
+            with open(f"qrcode/{i[0]}.qr.png", "wb") as Q:
+                qr_code = generate_qrcode(args.size, i)
+                Q.write(qr_code.content)
+            logger.debug("Created QRcode for %s", i[0])
+        logger.info("Created QRs for all")
 
 
 def create_database(args, user):
@@ -361,7 +347,7 @@ def export_employee_details(data):
         for row in data:
             writer.writerow(row)
             logger.debug("Writed row on csv file")
-    logger.info("Exported ")
+    logger.info("Exported File as employee_summary.csv ")
 
 
 def main():
--- test_vcard.py
+++ test_vcard.py
@@ -1,8 +1,11 @@
 from hr import parse_data,generate_vcard_content
+from mock import Mock
 
 def test_parsed_data():
-    file = "test_files/test_sample.csv"
-    assert parse_data(file) == [['Campbell', 'Pamela', 'Hydrologist', 'pamel.campb@wilson.com', '001-371-295-6798x4774'],
+    args = Mock()
+    args.load = "test_files/test_sample.csv"
+    
+    assert parse_data(args) == [['Campbell', 'Pamela', 'Hydrologist', 'pamel.campb@wilson.com', '001-371-295-6798x4774'],
                                ['Ayers', 'Adam', 'Clinical cytogeneticist', 'adam.ayers@lopez-wu.biz', '+1-407-869-4881']]
                                 
 
