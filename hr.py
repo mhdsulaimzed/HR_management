82cb011d955c6a62caf8c54aecd17afbf627ffd4
sulaim <sulaimsaed@gmail.com>
2023-12-05 02:04:05 +0530

    altered load,qr and vcard functions

--- hr.py
+++ hr.py
@@ -34,17 +34,19 @@ def configure_logger(args):
     logger.addHandler(handler)
     logger.addHandler(fhandler)
 
+
 def set_db_config(dbname):
     config = ConfigParser()
-    config.read('init.ini')
-    config.set("Database","dbname",dbname)
-    with open('init.ini',"w") as configfile:
+    config.read("init.ini")
+    config.set("Database", "dbname", dbname)
+    with open("init.ini", "w") as configfile:
         config.write(configfile)
+
 
 def parse_args():
     config = ConfigParser()
     config.read("init.ini")
-    print(config["Database"]["dbname"])
+
     todays_date = str(datetime.date.today())
 
     parser = argparse.ArgumentParser(
@@ -54,7 +56,11 @@ def parse_args():
     )
 
     parser.add_argument(
-        "-b", "--db", help="Database name (Default : %(default)s)", action="store_true" , default=config["Database"]["dbname"]
+        "-b",
+        "--db",
+        help="Database name (Default : %(default)s)",
+        action="store_true",
+        default=config["Database"]["dbname"],
     )
     parser.add_argument(
         "-v", "--verbose", help="print out detailed logs", action="store_true"
@@ -64,14 +70,19 @@ def parse_args():
 
     # database command
     parser_createdb = subparser.add_parser("createdb", help="create a database")
-    parser_createdb.add_argument("-b", "--db", help="database name", type=str)
 
     # loading csv into database
     parser_load = subparser.add_parser(
-        "loadcsv", help="insert data from file into employee table in database ",description="Imports list of employees into the database from specified file '-l'"
+        "loadcsv",
+        help="insert data from file into employee table in database ",
+        description="Imports list of employees into the database from specified file '-l'",
     )
-    parser_load.add_argument("file",help="File name ",type=str,)
-   
+    parser_load.add_argument(
+        "file",
+        help="File name ",
+        type=str,
+    )
+
     parser_load.add_argument(
         "-d",
         "--address",
@@ -82,46 +93,64 @@ def parse_args():
 
     # commad for creating vcards
     parser_vcard = subparser.add_parser("vcard", help="generate vcards")
-    
+    parser_vcard.add_argument(
+        "-d",
+        "--address",
+        help="Add a new address",
+        type=str,
+        default="100 Flat Grape Dr.;Fresno;CA;95555;United States of America",
+    )
 
     parser_qrcode = subparser.add_parser("qrcode", help="generate qrcode")
-    parser_qrcode.add_argument("-s","--size",
+    parser_qrcode.add_argument(
+        "-s",
+        "--size",
         help="Qr dimension ",
-        default=300,)
+        default=300,
+    )
 
-    parser_leave_emp = subparser.add_parser("leave-emp", help="add leaves for employees",description="Adds leaves taken by the employee")
-
+    parser_leave_emp = subparser.add_parser(
+        "leave-emp",
+        help="add leaves for employees",
+        description="Adds leaves taken by the employee",
+    )
     parser_leave_emp.add_argument("empid", help="Employee id ", type=str)
-    parser_leave_emp.add_argument("-d", "--date", help="Date 'YYYY-MM-DD' (Default : %(default)s) ", type=str, default=todays_date)
-    parser_leave_emp.add_argument( "-r", "--reason",help=" Reason for leave ",type=str,default="Not mentioned")
-    parser_export = subparser.add_parser("export", help="Get the employee detail as csv file",description="Get a detailed summary of all employees in a csv file")
-    parser_export.add_argument("-f","--file",)
+    parser_leave_emp.add_argument(
+        "-d",
+        "--date",
+        help="Date 'YYYY-MM-DD' (Default : %(default)s) ",
+        type=str,
+        default=todays_date,
+    )
+    parser_leave_emp.add_argument(
+        "-r", "--reason", help=" Reason for leave ", type=str, default="Not mentioned"
+    )
+    parser_export = subparser.add_parser(
+        "export",
+        help="Get the employee detail as csv file",
+        description="Get a detailed summary of all employees in a csv file",
+    )
+    parser_export.add_argument(
+        "-f",
+        "--file",
+    )
 
     args = parser.parse_args()
     return args
 
 
-
-
-
-def fetch_from_db(args, user):
-    try:
-        connection = psycopg2.connect(f"dbname={args.db} user={user}")
-        curs = connection.cursor()
-        curs.execute(
-            f"""SELECT first_name,last_name,designation,email,phone,company_address
-                         FROM
-                         employees """
-        )
-        data = [list(row) for row in curs.fetchall()]
-        connection.commit()
-        curs.close()
-        connection.close()
-        logger.info(f"Fetched all data from employees table in {args.db} database")
-        return data
-    except psycopg2.OperationalError as e:
-        logger.error(e)
-        sys.exit(-1)
+def fetch_from_db(args):
+    db_uri = f"postgresql:///{args.db}"
+    session = models.get_session(db_uri)
+    qs = sa.select(
+        models.Employee.fname,
+        models.Employee.lname,
+        models.Designation.title,
+        models.Employee.email,
+        models.Employee.phone,
+    ).where(models.Employee.title_id == models.Designation.id)
+    data = session.execute(qs).fetchall()
+    return data
 
 
 def generate_vcard_content(lname, fname, designation, email, phone, address):
@@ -146,13 +175,15 @@ def generate_qrcode(dimension, i):
     return qr_code
 
 
-def create_vcards(data):
+def create_vcards(data, args):
     if os.path.exists("vcard"):
         shutil.rmtree("vcard")
-    os.mkdir("vcards")
+    os.mkdir("vcard")
     for i in data:
-        lname, fname, designation, email, phone, address = i
-        vcard = generate_vcard_content(lname, fname, designation, email, phone, address)
+        lname, fname, designation, email, phone = i
+        vcard = generate_vcard_content(
+            lname, fname, designation, email, phone, args.address
+        )
         with open(f"vcards/{lname}.vcf", "w") as d:
             d.write(vcard)
         logger.debug("Created vcard for %s ", lname)
@@ -162,35 +193,34 @@ def create_vcards(data):
 def create_qrcode_images(data, args):
     if type(args.size) != int or int(args.size) > 500:
         logger.error("size must be an integer between 100 - 500")
-    
+
     else:
         if os.path.exists("qrcode"):
-            shutil.rmtree("vcard")
+            shutil.rmtree("qrcode")
         os.mkdir("qrcode")
+        logger.info("Wait this may take few minutes...")
         for i in data:
             with open(f"qrcode/{i[0]}.qr.png", "wb") as Q:
                 qr_code = generate_qrcode(args.size, i)
                 Q.write(qr_code.content)
             logger.debug("Created QRcode for %s", i[0])
-        logger.info("Created QRs for all")
+        logger.info("Created QRs for all in qrcode folder")
 
 
 def create_database(args):
-    try: 
-        db_uri = f"postgresql:///{args.dbname}"
-        models.create_all(db_uri)
-        session = models.get_session(db_uri)
-        s1 = models.Designation(title = 'Staff Engineer',max_levaes=20)
-        s2 = models.Designation(title = 'Senior Engineer',max_levaes=20)
-        s3 = models.Designation(title = 'Junior Engineer',max_levaes=20)
-        s4 = models.Designation(title = 'Tech Lead',max_levaes=20)
-        s5 = models.Designation(title = 'Project Manager',max_levaes=20)
-        session.add_all(["s1","s2","s3","s4","s5"])
-        session.commit()  
-    except psycopg2.errors.DuplicateTable as e:
-        logger.error("Table already exists: %s", e)
-        sys.exit(-1)
-    
+    db_uri = f"postgresql:///{args.db}"
+    models.create_all(db_uri)
+    session = models.get_session(db_uri)
+    objects = [
+        models.Designation(title="Staff Engineer", max_leaves=20),
+        models.Designation(title="Senior Engineer", max_leaves=20),
+        models.Designation(title="Junior Engineer", max_leaves=20),
+        models.Designation(title="Tech Lead", max_leaves=20),
+        models.Designation(title="Project Manager", max_leaves=20),
+    ]
+    session.add_all(objects)
+    session.commit()
+    logger.info("Datebase Created ")
 
 
 def parse_data(args):
@@ -206,38 +236,50 @@ def parse_data(args):
     except FileNotFoundError as e:
         logger.error("Error %s", (e,))
         sys.exit(-1)
-    
 
 
 def load_csv_into_db(args, data):
-        
-    db_uri = f"postgresql:///{args.dbname}"
+    db_uri = f"postgresql:///{args.db}"
     session = models.get_session(db_uri)
+    print(data)
     fname, lname, title, email, phone = data
-    qs = sa.select(models.Designation).where(models.Designation.title == title)
-    designation = session.execute(qs)
-    employee = models.Employee(lname=lname,
-                                   fname=fname,
-                                   title=designation,
-                                   email=email,
-                                   phone=phone)
-    session.add(employee)
+    s = models.Employee(
+        fname=fname, lname=lname, title_id=(title), email=email, phone=phone
+    )
+    session.add(s)
     session.commit()
+    logger.debug(f"Inserted into datase: {fname}")
 
 
 def load_leave_employee(args):
-    db_uri = f"postgresql:///{args.dbname}"
+    db_uri = f"postgresql:///{args.db}"
     session = models.get_session(db_uri)
     qs = sa.select(models.Leave).where(models.Leave.employee_id == args.empid)
     employee_on_leave = session.execute(qs)
-    leave_input = models.Leave(date = args.date,employee_id = args.empid,reason = args.reason)
+    leave_input = models.Leave(
+        date=args.date, employee_id=args.empid, reason=args.reason
+    )
 
     session.add(leave_input)
     session.commit()
+    logger.info(f"Leave added {args.empid}")
 
 
 def join_tables(args, user):
     try:
+        db_uri = f"postgresql:///{args.dbname}"
+        session = models.get_session(db_uri)
+        qs = sa.select(
+            models.Employee.id,
+            models.Employee.fname,
+            models.Employee.lname,
+            models.Employee.email,
+            models.Employee.phone,
+        ).where(
+            models.Designation.id == models.Employee.title_id,
+            models.Leave.employee_id == args.empid,
+        )
+        pp = session.execute(qs)
         connection = psycopg2.connect(f"dbname={args.db} user={user}")
         curs = connection.cursor()
         curs.execute(
@@ -300,23 +342,24 @@ def main():
     if args.subcommand == "createdb":
         set_db_config(args.db)
         create_database(args)
-        
 
     if args.subcommand == "loadcsv":
         data = parse_data(args)
         for i in range(len(data)):
-            load_csv_into_db(args, data[i], user)
+            load_csv_into_db(args, data[i])
+        logger.info("Inserted all datas into database")
 
     if args.subcommand == "vcard":
-        data = fetch_from_db(args, user)
-        create_vcards(data)
+        data = fetch_from_db(args)
+        print(data)
+        create_vcards(data, args)
 
     if args.subcommand == "qrcode":
-        data = fetch_from_db(args, user)
+        data = fetch_from_db(args)
         create_qrcode_images(data, args)
 
-    if args.subcommand == "leavemp":
-        load_leave_employee(args, user)
+    if args.subcommand == "leave-emp":
+        load_leave_employee(args)
 
     if args.subcommand == "export":
         data = join_tables(args, user)
--- models.py
+++ models.py
@@ -1,19 +1,20 @@
-from sqlalchemy import Integer,String,ForeignKey,Date
+from sqlalchemy import Integer,String,ForeignKey,Date,create_engine,UniqueConstraint
 from sqlalchemy.orm import DeclarativeBase,sessionmaker
 from sqlalchemy.orm import Mapped,mapped_column,relationship    
 import datetime
 import logging
+from typing import List
 
 class Base(DeclarativeBase):
         def __repr__(self):
             return f"{self.__class__.__name__}(id={self.id})"
 
 class Designation(Base):
-    __tablename__='hrms_designation'
+    __tablename__="hrms_designation"
     id : Mapped[int] = mapped_column(primary_key=True)
-    title : Mapped[str] = mapped_column(String(50))
-    max_leaves =Mapped[int] = mapped_column(Integer)
-    employee = relationship("Employee",back_populates="title")
+    title : Mapped[str] = mapped_column(String(50),unique=True)
+    max_leaves : Mapped[int] = mapped_column(Integer)
+    employee : Mapped[List["Employee"]] = relationship("Employee",back_populates="title")
 
 
 class Employee(Base):
@@ -21,25 +22,29 @@ class Employee(Base):
     id : Mapped[int] = mapped_column(primary_key=True)
     fname: Mapped[str] =  mapped_column(String(50))
     lname: Mapped[str] =  mapped_column(String(50))
-    email: Mapped[str] =  mapped_column(String(120))
-    phone: Mapped[str] =  mapped_column(String(50))
     title_id:Mapped[int] = mapped_column(ForeignKey('hrms_designation.id'))
-    title=relationship("Designation",backref="employee")
+    email: Mapped[str] =  mapped_column(String(120),unique=True)
+    phone: Mapped[str] =  mapped_column(String(50))
+
+    title :Mapped["Designation"] =relationship("Designation",back_populates="employee")
 
 class Leave(Base):
     __tablename__="hrms_employee_leaves"
+    __table_args__ = (        
+        UniqueConstraint("employee_id", "date"),
+        )
     id : Mapped[int] = mapped_column(primary_key=True)
     date : Mapped[datetime.date] = mapped_column(Date())
     employee_id : Mapped[int] = mapped_column(ForeignKey('hrms_employees.id'))
     reason : Mapped[str] = mapped_column(String(120))
 
-def create_engine(db_uri):
+def create_all(db_uri):
      logger = logging.getLogger('HR')
      engine = create_engine(db_uri)
      Base.metadata.create_all(engine)
      logging.info('Schema Created')
 
-def session(db_uri):
+def get_session(db_uri):
      engine = create_engine(db_uri)
      Session = sessionmaker(bind=engine)
      session = Session()
